0fd8e1d9843a20e04a98ea06a5dd801ca787ad64
Guy Wilson <wilson.guy@gmail.com>
2019-07-17 10:18:44 +0100

    Added rpi header file

---
+++ src/rpi.h
@@ -0,0 +1,16 @@
+#ifndef _INCL_RPI
+#define _INCL_RPI
+
+#include "./types.h"
+#include "./exception.h"
+#include "./register.h"
+#include "./clock.h"
+#include "./peripheral.h"
+#include "./gpio.h"
+#include "./pwm.h"
+#include "./spi.h"
+#include "./swtimer.h"
+#include "./pifactory.h"
+#include "./rasppi.h"
+
+#endif
